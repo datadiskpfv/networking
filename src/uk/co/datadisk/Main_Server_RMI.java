48a1e7dde9450bbdca76641c6126ccefe1f3a23e
pvalle <paul.valle@orbian.com>
2018-10-01 11:38:02 +0100

    Java Networking URI, URL and Apache HTTP Client, also looking passing objects, now added RMI example

---
+++ src/uk/co/datadisk/Main_Server_RMI.java
@@ -0,0 +1,32 @@
+package uk.co.datadisk;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Main_Server_RMI extends UnicastRemoteObject implements RMIInterface {
+
+    private static final long serialVersionUID = 1L;
+
+    protected Main_Server_RMI() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public String helloTo(String name) throws RemoteException {
+        System.err.println(name + " is trying to contact");
+        return "Server says hello to " + name;
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            Naming.rebind("//localhost/MyServer", new Main_Server_RMI());
+            System.err.println("Server ready");
+
+        } catch (Exception e){
+            System.out.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
