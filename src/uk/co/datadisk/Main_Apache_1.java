e3544d5cdb661fcaedd6980b80dc42b5fe4e4151
pvalle <paul.valle@orbian.com>
2018-10-01 08:47:27 +0100

    Java Networking URI, URL and Apache HTTP Client

---
+++ src/uk/co/datadisk/Main_Apache_1.java
@@ -0,0 +1,41 @@
+package uk.co.datadisk;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_Apache_1 {
+
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet request = new HttpGet("http://example.org");
+        request.addHeader("User-Agent", "Chrome");
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        try {
+            System.out.println("STATUS Code: " + response.getStatusLine().getStatusCode());
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            String line;
+
+            while ((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            inputReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            response.close();
+        }
+    }
+}
--- src/uk/co/datadisk/Main_URL_2.java
+++ src/uk/co/datadisk/Main_URL_2.java
@@ -1,6 +1,8 @@
 package uk.co.datadisk;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -15,8 +17,25 @@ public class Main_URL_2 {
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();  // returns an open connection instance, give you chance to setup connection settings
 
             urlConnection.setRequestMethod("GET");    // this is optional
+            urlConnection.setRequestProperty("User-Agent", "Chrome");
+            urlConnection.setReadTimeout(30000);
 
+            int responseCode = urlConnection.getResponseCode();
+            System.out.println("STATUS Code: " + responseCode);
 
+            if(responseCode != 200){
+                System.out.println("ERROR: reading web page");
+            }
+
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            String line;
+
+            while((line = inputReader.readLine()) != null){
+                System.out.println(line);
+            }
+
+            inputReader.close();
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
